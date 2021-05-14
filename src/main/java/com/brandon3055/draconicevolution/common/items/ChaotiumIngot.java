eabade392f4a3960b31510b4fc2b8903c17de5e0
TheGizza <GizzaGanderDima@gmail.com>
2021-05-15 00:37:27 +0300

    lang update

--- src/main/java/com/brandon3055/draconicevolution/common/items/ChaotiumIngot.java
+++ src/main/java/com/brandon3055/draconicevolution/common/items/ChaotiumIngot.java
@@ -3,11 +3,22 @@ package com.brandon3055.draconicevolution.common.items;
 import com.Mellenium.Addons.MelleniumAddons;
 import com.brandon3055.draconicevolution.common.ModItems2;
 import com.brandon3055.draconicevolution.common.lib.Strings2;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
 
 public class ChaotiumIngot extends ItemDE2 {
     public ChaotiumIngot() {
         this.setUnlocalizedName(Strings2.chaotiumIngotName);
         this.setCreativeTab(MelleniumAddons.tabMelleniumAds);
         ModItems2.register(this);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean extended) {
+        list.add("Hello");
     }
 }
