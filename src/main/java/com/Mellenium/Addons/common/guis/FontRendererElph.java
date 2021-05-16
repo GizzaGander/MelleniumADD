a60b178d2a267b864be74e2444b40d201d1937bf
Danil Kolensikov <d.kolesnikov@innopolis.university>
2021-05-17 00:44:22 +0300

    clientfixer fix...

--- src/main/java/com/Mellenium/Addons/common/guis/FontRendererElph.java
+++ src/main/java/com/Mellenium/Addons/common/guis/FontRendererElph.java
@@ -3,6 +3,7 @@ package com.Mellenium.Addons.common.guis;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.settings.GameSettings;
+import net.minecraft.item.ItemBook;
 import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 
@@ -13,9 +14,64 @@ import java.io.IOException;
 public class FontRendererElph extends FontRenderer {
 
     private final int[] elphCharWidth = new int[256];
+    private final boolean unicodeFlag;
+    private float red;
+    private float blue;
+    private float green;
+    private float alpha;
+    private boolean bidiFlag;
+    private boolean randomStyle;
+    private boolean boldStyle;
+    private boolean italicStyle;
+    private boolean underlineStyle;
+    private boolean strikethroughStyle;
+
+    private int[] colorCode = new int[32];
+    private int textColor;
+
 
     public FontRendererElph(GameSettings p_i1035_1_, ResourceLocation p_i1035_2_, TextureManager p_i1035_3_, boolean p_i1035_4_) {
         super(p_i1035_1_, p_i1035_2_, p_i1035_3_, p_i1035_4_);
+        unicodeFlag = p_i1035_4_;
+        this.fillCharWidths();
+        this.fillColorCodes();
+    }
+
+    private void fillColorCodes() {
+        for (int i = 0; i < 32; ++i)
+        {
+            int j = (i >> 3 & 1) * 85;
+            int k = (i >> 2 & 1) * 170 + j;
+            int l = (i >> 1 & 1) * 170 + j;
+            int i1 = (i >> 0 & 1) * 170 + j;
+
+            if (i == 6)
+            {
+                k += 85;
+            }
+
+            /*if (p_i1035_1_.anaglyph)
+            {
+                int j1 = (k * 30 + l * 59 + i1 * 11) / 100;
+                int k1 = (k * 30 + l * 70) / 100;
+                int l1 = (k * 30 + i1 * 70) / 100;
+                k = j1;
+                l = k1;
+                i1 = l1;
+            }*/
+
+            if (i >= 16)
+            {
+                k /= 4;
+                l /= 4;
+                i1 /= 4;
+            }
+
+            this.colorCode[i] = (k & 255) << 16 | (l & 255) << 8 | i1 & 255;
+        }
+    }
+
+    private void fillCharWidths(){
         BufferedImage bufferedimage;
 
         try
@@ -26,6 +82,7 @@ public class FontRendererElph extends FontRenderer {
         {
             throw new RuntimeException(ioexception);
         }
+
         final int singleCharWidth = bufferedimage.getWidth()/32/2;
 
         for(int i = 0; i < this.elphCharWidth.length; i++){
@@ -87,6 +144,239 @@ public class FontRendererElph extends FontRenderer {
         bindTexture(this.locationFontTexture);
     }
 
+    private void resetStyles()
+    {
+        this.randomStyle = false;
+        this.boldStyle = false;
+        this.italicStyle = false;
+        this.underlineStyle = false;
+        this.strikethroughStyle = false;
+    }
+
+    @Override
+    public int drawString(String p_85187_1_, int p_85187_2_, int p_85187_3_, int p_85187_4_, boolean p_85187_5_)
+    {
+        enableAlpha();
+        this.resetStyles();
+        int l;
+
+        if (p_85187_5_)
+        {
+            l = this.renderString(p_85187_1_, p_85187_2_ + 1, p_85187_3_ + 1, p_85187_4_, true);
+            l = Math.max(l, this.renderString(p_85187_1_, p_85187_2_, p_85187_3_, p_85187_4_, false));
+        }
+        else
+        {
+            l = this.renderString(p_85187_1_, p_85187_2_, p_85187_3_, p_85187_4_, false);
+        }
+
+        return l;
+    }
+
+    private int renderString(String p_78258_1_, int p_78258_2_, int p_78258_3_, int p_78258_4_, boolean p_78258_5_)
+    {
+        if (p_78258_1_ == null)
+        {
+            return 0;
+        }
+        else
+        {
+          /*  if (this.bidiFlag)
+            {
+                p_78258_1_ = this.bidiReorder(p_78258_1_);
+            }*/
+
+            if ((p_78258_4_ & -67108864) == 0)
+            {
+                p_78258_4_ |= -16777216;
+            }
+
+            if (p_78258_5_)
+            {
+                p_78258_4_ = (p_78258_4_ & 16579836) >> 2 | p_78258_4_ & -16777216;
+            }
+
+            this.red = (float)(p_78258_4_ >> 16 & 255) / 255.0F;
+            this.blue = (float)(p_78258_4_ >> 8 & 255) / 255.0F;
+            this.green = (float)(p_78258_4_ & 255) / 255.0F;
+            this.alpha = (float)(p_78258_4_ >> 24 & 255) / 255.0F;
+            setColor(this.red, this.blue, this.green, this.alpha);
+            this.posX = (float)p_78258_2_;
+            this.posY = (float)p_78258_3_;
+            this.renderStringAtPos(p_78258_1_, p_78258_5_);
+            return (int)this.posX;
+        }
+    }
+
+    private void renderStringAtPos(String p_78255_1_, boolean p_78255_2_)
+    {
+        for (int i = 0; i < p_78255_1_.length(); ++i)
+        {
+            char c0 = p_78255_1_.charAt(i);
+            int j;
+            int k;
+
+            if (c0 == 167 && i + 1 < p_78255_1_.length())
+            {
+                j = "0123456789abcdefklmnor".indexOf(p_78255_1_.toLowerCase().charAt(i + 1));
+
+                if (j < 16)
+                {
+                    this.randomStyle = false;
+                    this.boldStyle = false;
+                    this.strikethroughStyle = false;
+                    this.underlineStyle = false;
+                    this.italicStyle = false;
+
+                    if (j < 0 || j > 15)
+                    {
+                        j = 15;
+                    }
+
+                    if (p_78255_2_)
+                    {
+                        j += 16;
+                    }
+
+                    k = this.colorCode[j];
+                    this.textColor = k;
+                    setColor((float)(k >> 16) / 255.0F, (float)(k >> 8 & 255) / 255.0F, (float)(k & 255) / 255.0F, this.alpha);
+                }
+                else if (j == 16)
+                {
+                    this.randomStyle = true;
+                }
+                else if (j == 17)
+                {
+                    this.boldStyle = true;
+                }
+                else if (j == 18)
+                {
+                    this.strikethroughStyle = true;
+                }
+                else if (j == 19)
+                {
+                    this.underlineStyle = true;
+                }
+                else if (j == 20)
+                {
+                    this.italicStyle = true;
+                }
+                else if (j == 21)
+                {
+                    this.randomStyle = false;
+                    this.boldStyle = false;
+                    this.strikethroughStyle = false;
+                    this.underlineStyle = false;
+                    this.italicStyle = false;
+                    setColor(this.red, this.blue, this.green, this.alpha);
+                }
+
+                ++i;
+            }
+            else
+            {
+                j = "\u00c0\u00c1\u00c2\u00c8\u00ca\u00cb\u00cd\u00d3\u00d4\u00d5\u00da\u00df\u00e3\u00f5\u011f\u0130\u0131\u0152\u0153\u015e\u015f\u0174\u0175\u017e\u0207\u0000\u0000\u0000\u0000\u0000\u0000\u0000 !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u0000\u00c7\u00fc\u00e9\u00e2\u00e4\u00e0\u00e5\u00e7\u00ea\u00eb\u00e8\u00ef\u00ee\u00ec\u00c4\u00c5\u00c9\u00e6\u00c6\u00f4\u00f6\u00f2\u00fb\u00f9\u00ff\u00d6\u00dc\u00f8\u00a3\u00d8\u00d7\u0192\u00e1\u00ed\u00f3\u00fa\u00f1\u00d1\u00aa\u00ba\u00bf\u00ae\u00ac\u00bd\u00bc\u00a1\u00ab\u00bb\u2591\u2592\u2593\u2502\u2524\u2561\u2562\u2556\u2555\u2563\u2551\u2557\u255d\u255c\u255b\u2510\u2514\u2534\u252c\u251c\u2500\u253c\u255e\u255f\u255a\u2554\u2569\u2566\u2560\u2550\u256c\u2567\u2568\u2564\u2565\u2559\u2558\u2552\u2553\u256b\u256a\u2518\u250c\u2588\u2584\u258c\u2590\u2580\u03b1\u03b2\u0393\u03c0\u03a3\u03c3\u03bc\u03c4\u03a6\u0398\u03a9\u03b4\u221e\u2205\u2208\u2229\u2261\u00b1\u2265\u2264\u2320\u2321\u00f7\u2248\u00b0\u2219\u00b7\u221a\u207f\u00b2\u25a0\u0000".indexOf(c0);
+
+                if (this.randomStyle && j != -1)
+                {
+                    do
+                    {
+                        k = this.fontRandom.nextInt(this.charWidth.length);
+                    }
+                    while (this.charWidth[j] != this.charWidth[k]);
+
+                    j = k;
+                }
+
+                float f1 = this.unicodeFlag ? 0.5F : 1.0F;
+                boolean flag1 = (c0 == 0 || j == -1 || this.unicodeFlag) && p_78255_2_;
+
+                if (flag1)
+                {
+                    this.posX -= f1;
+                    this.posY -= f1;
+                }
+
+                float f = this.renderCharAtPos(j, c0, this.italicStyle);
+
+                if (flag1)
+                {
+                    this.posX += f1;
+                    this.posY += f1;
+                }
+
+                if (this.boldStyle)
+                {
+                    this.posX += f1;
+
+                    if (flag1)
+                    {
+                        this.posX -= f1;
+                        this.posY -= f1;
+                    }
+
+                    this.renderCharAtPos(j, c0, this.italicStyle);
+                    this.posX -= f1;
+
+                    if (flag1)
+                    {
+                        this.posX += f1;
+                        this.posY += f1;
+                    }
+
+                    ++f;
+                }
+
+                doDraw(f);
+            }
+        }
+    }
+
+    private float renderCharAtPos(int p_78278_1_, char p_78278_2_, boolean p_78278_3_)
+    {
+        return p_78278_2_ == 32 ? 4.0F : ("\u00c0\u00c1\u00c2\u00c8\u00ca\u00cb\u00cd\u00d3\u00d4\u00d5\u00da\u00df\u00e3\u00f5\u011f\u0130\u0131\u0152\u0153\u015e\u015f\u0174\u0175\u017e\u0207\u0000\u0000\u0000\u0000\u0000\u0000\u0000 !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u0000\u00c7\u00fc\u00e9\u00e2\u00e4\u00e0\u00e5\u00e7\u00ea\u00eb\u00e8\u00ef\u00ee\u00ec\u00c4\u00c5\u00c9\u00e6\u00c6\u00f4\u00f6\u00f2\u00fb\u00f9\u00ff\u00d6\u00dc\u00f8\u00a3\u00d8\u00d7\u0192\u00e1\u00ed\u00f3\u00fa\u00f1\u00d1\u00aa\u00ba\u00bf\u00ae\u00ac\u00bd\u00bc\u00a1\u00ab\u00bb\u2591\u2592\u2593\u2502\u2524\u2561\u2562\u2556\u2555\u2563\u2551\u2557\u255d\u255c\u255b\u2510\u2514\u2534\u252c\u251c\u2500\u253c\u255e\u255f\u255a\u2554\u2569\u2566\u2560\u2550\u256c\u2567\u2568\u2564\u2565\u2559\u2558\u2552\u2553\u256b\u256a\u2518\u250c\u2588\u2584\u258c\u2590\u2580\u03b1\u03b2\u0393\u03c0\u03a3\u03c3\u03bc\u03c4\u03a6\u0398\u03a9\u03b4\u221e\u2205\u2208\u2229\u2261\u00b1\u2265\u2264\u2320\u2321\u00f7\u2248\u00b0\u2219\u00b7\u221a\u207f\u00b2\u25a0\u0000".indexOf(p_78278_2_) != -1 && !this.unicodeFlag ? this.renderDefaultChar(p_78278_1_, p_78278_3_) : this.renderUnicodeChar(p_78278_2_, p_78278_3_));
+    }
+
+    @Override
+    public int getCharWidth(char p_78263_1_)
+    {
+        if (p_78263_1_ == 167)
+        {
+            return -1;
+        }
+        else if (p_78263_1_ == 32)
+        {
+            return 4;
+        }
+        else
+        {
+            int i = "\u00c0\u00c1\u00c2\u00c8\u00ca\u00cb\u00cd\u00d3\u00d4\u00d5\u00da\u00df\u00e3\u00f5\u011f\u0130\u0131\u0152\u0153\u015e\u015f\u0174\u0175\u017e\u0207\u0000\u0000\u0000\u0000\u0000\u0000\u0000 !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u0000\u00c7\u00fc\u00e9\u00e2\u00e4\u00e0\u00e5\u00e7\u00ea\u00eb\u00e8\u00ef\u00ee\u00ec\u00c4\u00c5\u00c9\u00e6\u00c6\u00f4\u00f6\u00f2\u00fb\u00f9\u00ff\u00d6\u00dc\u00f8\u00a3\u00d8\u00d7\u0192\u00e1\u00ed\u00f3\u00fa\u00f1\u00d1\u00aa\u00ba\u00bf\u00ae\u00ac\u00bd\u00bc\u00a1\u00ab\u00bb\u2591\u2592\u2593\u2502\u2524\u2561\u2562\u2556\u2555\u2563\u2551\u2557\u255d\u255c\u255b\u2510\u2514\u2534\u252c\u251c\u2500\u253c\u255e\u255f\u255a\u2554\u2569\u2566\u2560\u2550\u256c\u2567\u2568\u2564\u2565\u2559\u2558\u2552\u2553\u256b\u256a\u2518\u250c\u2588\u2584\u258c\u2590\u2580\u03b1\u03b2\u0393\u03c0\u03a3\u03c3\u03bc\u03c4\u03a6\u0398\u03a9\u03b4\u221e\u2205\u2208\u2229\u2261\u00b1\u2265\u2264\u2320\u2321\u00f7\u2248\u00b0\u2219\u00b7\u221a\u207f\u00b2\u25a0\u0000".indexOf(p_78263_1_);
+
+            if (p_78263_1_ > 0 && i != -1 && !this.unicodeFlag || p_78263_1_ >= 'а' && p_78263_1_ <= 'я' || p_78263_1_ >= 'А' && p_78263_1_ <= 'Я')
+            {
+                return this.elphCharWidth[i];
+            }
+            else if (this.glyphWidth[p_78263_1_] != 0)
+            {
+                int j = this.glyphWidth[p_78263_1_] >>> 4;
+                int k = this.glyphWidth[p_78263_1_] & 15;
+
+                if (k > 7)
+                {
+                    k = 15;
+                    j = 0;
+                }
+
+                ++k;
+                return (k - j) / 2 + 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
 
     @Override
     protected float renderDefaultChar(int p_78266_1_, boolean p_78266_2_) {
